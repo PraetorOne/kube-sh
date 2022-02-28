9a4a3e8ad5bf7da0b67d40aa1e298bfd44e34a00
Jigar Patel <jigar@arc10.io>
2022-02-28 10:58:20 -0500

    Added two independent akash scrips

---
+++ akash.sh
@@ -0,0 +1,42 @@
+!/bin/bash
+stable=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/${stable}/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+mv ./kubectl /usr/local/bin/kubectl
+
+kubectl label nodes node1 akash.network/role=ingress
+
+kubectl apply -f https://raw.githubusercontent.com/ovrclk/akash/mainnet/main/pkg/apis/akash.network/v1/crd.yaml
+kubectl apply -f https://raw.githubusercontent.com/ovrclk/akash/mainnet/main/pkg/apis/akash.network/v1/provider_hosts_crd.yaml
+
+kubectl get crd -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/ovrclk/akash/mainnet/main/_docs/kustomize/networking/network-policy-default-ns-deny.yaml
+
+git clone --depth 1 -b mainnet/main https://github.com/ovrclk/akash.git
+cd akash
+kubectl apply -f _docs/kustomize/networking/namespace.yaml
+kubectl kustomize _docs/kustomize/akash-services/ | kubectl apply -f -
+
+cat >> _docs/kustomize/akash-hostname-operator/kustomization.yaml <<'EOF'
+images:
+  - name: ghcr.io/ovrclk/akash:stable
+    newName: ghcr.io/ovrclk/akash
+    newTag: 0.14.1
+EOF
+
+kubectl kustomize _docs/kustomize/akash-hostname-operator | kubectl apply -f -
+
+kubectl apply -f https://raw.githubusercontent.com/ovrclk/akash/mainnet/main/_run/ingress-nginx-class.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/ovrclk/akash/mainnet/main/_run/ingress-nginx.yaml
+
+# check if ubuntu
+apt-get install unzip
+
+AKASH_VERSION="$(curl -s "https://raw.githubusercontent.com/ovrclk/net/master/mainnet/version.txt")"
+
+curl https://raw.githubusercontent.com/ovrclk/akash/master/godownloader.sh | sh -s -- "v$AKASH_VERSION"
---
+++ bash.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+#This bootstrap makes some assumptions:
+#1 : 3 new bare-metal servers using Debian 11 with root user and ssh password login enabled.
+#2 : A control machine to run this bootstrap from and control your new cluster.  The control machine needs all the dependencies in the depends function.
+#3 : Update USER, SSHPASS, NODE1, NODE2, NODE3 with your servers info.  You can add as many nodes as you like, just use the same format. "export NODEX=x.x.x.x"
+
+function apt_depends(){
+apt-get update ; apt-get install -y python3-pip git sshpass software-properties-common unzip
+add-apt-repository --yes --update ppa:ansible/ansible
+# apt-get install -y ansible
+apt install libffi-dev
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+git clone https://github.com/kubernetes-sigs/kubespray.git ; cd kubespray
+pip3 install -r requirements.txt
+}
+
+function yum_depends() {
+yum update
+yum install epel-release
+yum install -y python3-pip git sshpass software-properties-common libffi libffi-devel unzip
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+git clone https://github.com/kubernetes-sigs/kubespray.git ; cd kubespray
+pip3 install -r requirements.txt
+}
+
+export OS_RELEASE=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+export CENT_OS='"CentOS Linux"'
+export DEBIAN='"Debian GNU/Linux"'
+export UBUNTU='"Ubuntu"'
+
+if [ "$CENT_OS" = "$OS_RELEASE" ];
+then
+    yum_depends
+else
+    apt_depends
+fi
+
+
+###Server settings
+# USER=root #user on nodes to use
+# export SSHPASS=chilblaain #password to use to sync ssh keys
+# export NODE1=3.99.170.165 #node IP
+# export NODE2=3.98.139.185 #node IP
+# export NODE3=35.183.22.64 #node IP
+####
+
+echo "Please enter username"
+read USER
+
+echo "Please enter password of all nodes"
+read pass
+
+export SSHPASS=$pass
+
+echo "How many nodes are your connecting?"
+read NODECOUNT
+
+# echo "Please enter Node IP Separated by space"
+# read -a IPS
+
+ssh-keygen -t rsa -C $(hostname) -f "$HOME/.ssh/id_rsa" -P "" ; cat ~/.ssh/id_rsa.pub
+
+
+
+#Assign static IP to your Kubernetes Hosts and update the user and IP address below
+declare -a IPS
+for i in $(seq $NODECOUNT)
+do
+   echo "Please enter Node ${i} ip address"
+   read dest
+   IPS+=" ${dest}"
+   ssh-keyscan -H $dest >> ~/.ssh/known_hosts
+   sshpass -e ssh-copy-id -i ~/.ssh/id_rsa.pub $USER@$dest
+done
+unset SSHPASS
+
+
+echo "using nodes ${IPS}..."
+
+function ansible(){
+#Setup ansible
+cp -rfp inventory/sample inventory/akash
+#Create config.yaml
+CONFIG_FILE=inventory/akash/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+cat inventory/akash/hosts.yaml
+#Enable gvisor for security
+ex inventory/akash/hosts.yaml <<eof
+2 insert
+  vars:
+    cluster_id: "1.0.0.1"
+    ansible_user: root
+    gvisor_enabled: true
+.
+xit
+eof
+echo "File Modified"
+cat inventory/akash/hosts.yaml
+}
+ansible
+
+#Run Kubespray
+ansible-playbook -i inventory/akash/hosts.yaml -b -v --private-key=~/.ssh/id_rsa cluster.yml
+
+#Run Helm
+export KUBECONFIG=$KUBECONFIG
